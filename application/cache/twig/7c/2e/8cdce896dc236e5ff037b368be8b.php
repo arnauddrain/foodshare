c44ade69c417e7ee8c347956ab50d1cd3893f140
Jamukha <francois.pala@epitech.eu>
2014-09-09 14:14:00 +0200

    aze
    qsd

--- application/cache/twig/7c/2e/8cdce896dc236e5ff037b368be8b.php
+++ application/cache/twig/7c/2e/8cdce896dc236e5ff037b368be8b.php
@@ -28,40 +28,42 @@ class __TwigTemplate_7c2e8cdce896dc236e5ff037b368be8b extends Twig_Template
     public function block_content($context, array $blocks = array())
     {
         // line 3
-        echo "<div class=\"text-slider-wrap\" style=\"opacity: 1;\">
-  <div class=\"caroufredsel_wrapper\" style=\"display: block; text-align: start; float: none; position: relative; top: auto; right: auto; bottom: auto; left: auto; z-index: auto; width: 690px; height: 576px; margin: 0px; overflow: hidden;\">
-    <div class=\"text-slider\" style=\"text-align: left; float: none; position: absolute; top: 0px; right: auto; bottom: auto; left: 0px; margin: 0px; height: 2880px; width: 690px; z-index: auto;\">
-      <div class=\"slide\" style=\"height: 576px; padding-top: 123px;\">
-        <h2>Welcome to your<br>restaurant’s new website.</h2>
-        <p>It includes Boxy's exclusive <em>Slider Builder</em> so you can easily create some dynamic sliders like this one. In fact, the slider you see here is included! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-prev\"><i class=\"fa fa-arrow-up\"></i></a>
-        <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-next\"><i class=\"fa fa-arrow-down\"></i></a>
-        <a href=\"http://demo.boxystudio.com/garnish/menu-builder/\" class=\"bx-btn\">OUR MENU</a>
-      </div>
-      <div class=\"slide\" style=\"height: 576px; padding-top: 134px;\">
-        <h2>This is Another Slide</h2>
-        <p>You can add as many slides as you need. You can also change the position of the image and whether or not it gets rounded. Sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-prev\"><i class=\"fa fa-arrow-up\"></i></a>
-        <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-next\"><i class=\"fa fa-arrow-down\"></i></a>
-        <a href=\"http://demo.boxystudio.com/garnish/events/\" class=\"bx-btn\">OUR CALENDAR</a>
-      </div>
-    </div>
-  </div>
-</div>
-<div class=\"image-slider-wrap\" style=\"opacity: 1;\">
-  <div class=\"caroufredsel_wrapper\" style=\"display: block; text-align: start; float: none; position: relative; top: auto; right: auto; bottom: auto; left: auto; z-index: auto; width: 510px; height: 781px; margin: 0px; overflow: hidden;\">
-    <div class=\"image-slider\" style=\"text-align: left; float: none; position: absolute; top: 0px; right: auto; bottom: auto; left: 0px; margin: 0px; width: 2550px; height: 781px; z-index: auto; opacity: 1;\">
-      <div class=\"slide\" style=\"width: 510px;\">
-        <img src=\"./Garnish_files/image1.031a07b844-685x768.jpg\" alt=\"\" width=\"685\" id=\"image-id-3\">
-      </div>
-      <div class=\"slide\" style=\"width: 510px;\">
-        <img src=\"./Garnish_files/image2.72f4c6916c-685x774.jpg\" alt=\"\" width=\"685\" id=\"image-id-4\">
+        echo "<section id=\"slider\" style=\"height:576px;\" class=\"shell clearfix garnish\">
+    <div class=\"text-slider-wrap\" style=\"opacity: 1;\">
+      <div class=\"caroufredsel_wrapper\" style=\"display: block; text-align: start; float: none; position: relative; top: auto; right: auto; bottom: auto; left: auto; z-index: auto; width: 690px; height: 576px; margin: 0px; overflow: hidden;\">
+        <div class=\"text-slider\" style=\"text-align: left; float: none; position: absolute; top: 0px; right: auto; bottom: auto; left: 0px; margin: 0px; height: 2880px; width: 690px; z-index: auto;\">
+          <div class=\"slide\" style=\"height: 576px; padding-top: 123px;\">
+            <h2>Welcome to your<br>restaurant’s new website.</h2>
+            <p>It includes Boxy's exclusive <em>Slider Builder</em> so you can easily create some dynamic sliders like this one. In fact, the slider you see here is included! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-prev\"><i class=\"fa fa-arrow-up\"></i></a>
+            <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-next\"><i class=\"fa fa-arrow-down\"></i></a>
+            <a href=\"http://demo.boxystudio.com/garnish/menu-builder/\" class=\"bx-btn\">OUR MENU</a>
+          </div>
+          <div class=\"slide\" style=\"height: 576px; padding-top: 134px;\">
+            <h2>This is Another Slide</h2>
+            <p>You can add as many slides as you need. You can also change the position of the image and whether or not it gets rounded. Sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-prev\"><i class=\"fa fa-arrow-up\"></i></a>
+            <a href=\"http://demo.boxystudio.com/garnish/#\" class=\"bx-btn btn-next\"><i class=\"fa fa-arrow-down\"></i></a>
+            <a href=\"http://demo.boxystudio.com/garnish/events/\" class=\"bx-btn\">OUR CALENDAR</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-<script type=\"text/javascript\">var slider_auto_cycle_speed = 7000;</script>
-<!--<section id=\"feature-blocks\" class=\"shell clearfix\">
+    <div class=\"image-slider-wrap\" style=\"opacity: 1;\">
+      <div class=\"caroufredsel_wrapper\" style=\"display: block; text-align: start; float: none; position: relative; top: auto; right: auto; bottom: auto; left: auto; z-index: auto; width: 510px; height: 781px; margin: 0px; overflow: hidden;\">
+        <div class=\"image-slider\" style=\"text-align: left; float: none; position: absolute; top: 0px; right: auto; bottom: auto; left: 0px; margin: 0px; width: 2550px; height: 781px; z-index: auto; opacity: 1;\">
+          <div class=\"slide\" style=\"width: 510px;\">
+            <img src=\"./Garnish_files/image1.031a07b844-685x768.jpg\" alt=\"\" width=\"685\" id=\"image-id-3\">
+          </div>
+          <div class=\"slide\" style=\"width: 510px;\">
+            <img src=\"./Garnish_files/image2.72f4c6916c-685x774.jpg\" alt=\"\" width=\"685\" id=\"image-id-4\">
+          </div>
+        </div>
+      </div>
+    </div>
+    <script type=\"text/javascript\">var slider_auto_cycle_speed = 7000;</script>
+</section>
+<section id=\"feature-blocks\" class=\"shell clearfix\">
   <div class=\"inner-shell\">
     <div class=\"fblock third\">
       <h3>Have a Look at Our Menu</h3>
@@ -82,7 +84,7 @@ class __TwigTemplate_7c2e8cdce896dc236e5ff037b368be8b extends Twig_Template
       <a class=\"bx-btn sm iconed\" href=\"http://demo.boxystudio.com/garnish/#\"><i class=\"fa fa-coffee\"></i>Book Your Table</a>
     </div>
   </div>
-</section>-->
+</section>
 ";
     }
 
